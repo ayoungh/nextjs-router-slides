60f1b12b409c439bfba844fff51f4aefbff75338
Anthony Young <404328+ayoungh@users.noreply.github.com>
2024-05-01 08:40:03 +0100

    Update slides and add file tree

--- components/pitch/section-start.tsx
+++ components/pitch/section-start.tsx
@@ -9,7 +9,7 @@ export function SectionStart() {
         <div className="absolute bottom-auto mt-[150px] -ml-[35px] md:ml-0 md:mt-0 md:bottom-[650px] scale-50 md:scale-100">
   
         </div>
-        <h1 className="text-[110px] bottom-[250px] left-2 md:text-[426px] absolute md:right-0 md:bottom-8 md:left-auto">
+        <h1 className="text-[90px] bottom-[250px] left-2 md:text-[426px] absolute md:right-0 md:bottom-8 md:left-auto">
           Welcome
         </h1>
       </div>
--- components/pitch/section-vision.tsx
+++ components/pitch/section-vision.tsx
@@ -7,12 +7,12 @@ export function SectionVision() {
       <div className="absolute left-8 right-8 top-4 flex justify-between text-lg">
         <span>Our vision</span>
         <span className="text-[#878787]">
-          <Link href="/">midday.ai</Link>
+          <Link href="/">Home</Link>
         </span>
       </div>
       <div className="flex flex-col min-h-screen justify-center container">
         <h1 className="text-[45px] px-4 md:px-0 md:text-[122px] font-medium text-center leading-none">
-          Our mission is to be the #1 OS for businesses.
+          To have easy to maintain code with a great developer experience.
         </h1>
       </div>
     </div>
