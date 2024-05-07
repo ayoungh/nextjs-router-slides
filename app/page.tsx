fef4b2c8cb6f9f3c08bb130c60de91ca07274af2
Anthony Young <404328+ayoungh@users.noreply.github.com>
2024-05-07 21:12:38 +0100

    add git link

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,16 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          {/* <a
+          <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://"
+            href="https://github.com/ayoungh/nextjs-router-slides/"
             target="_blank"
             rel="noopener noreferrer"
-          > */}
+          >
             By Anthony Young
-            {/* <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            /> */}
-          {/* </a> */}
+
+          </a>
         </div>
       </div>
 
